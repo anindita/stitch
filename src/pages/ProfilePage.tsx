67e33ccece1f685fe283990a26b27a62ba2a123f
rmp17 <rmp17@ic.ac.uk>
2021-04-17 19:01:45 +0100

    Added profile page to tab 3

---
+++ src/pages/ProfilePage.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {IonAvatar, IonItem, IonLabel, IonContent, IonTitle, IonBadge, IonIcon} from '@ionic/react';
+import {mail, people, star} from "ionicons/icons";
+
+export const ProfilePage: React.FC = () => (
+    <IonContent>
+        <IonItem>
+            <IonAvatar slot="start">
+                <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" />
+            </IonAvatar>
+            <IonTitle size="large" slot="start" >Rasika Navarange</IonTitle>
+        </IonItem>
+
+        {/*-- Items with badge on right --*/}
+        <IonItem>
+            <IonIcon icon={mail} slot="start" />
+            <IonLabel>Current Orders</IonLabel>
+            <IonBadge slot="end">6</IonBadge>
+        </IonItem>
+        <IonItem>
+            <IonIcon icon={people} slot="start" />
+            <IonLabel>Followers</IonLabel>
+            <IonBadge color="secondary" slot="end">203</IonBadge>
+        </IonItem>
+        <IonItem>
+            <IonIcon icon={star} slot="start" />
+            <IonLabel>Thimble points</IonLabel>
+            <IonBadge color="success" slot="end">200</IonBadge>
+        </IonItem>
+    </IonContent>
+);
+
+export default ProfilePage
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,6 +1,7 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
+import ProfilePage from "./ProfilePage";
 
 const Tab3: React.FC = () => {
   return (
@@ -11,12 +12,8 @@ const Tab3: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <ProfilePage>
+        </ProfilePage>
       </IonContent>
     </IonPage>
   );
