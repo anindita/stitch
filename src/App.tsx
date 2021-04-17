27c941acc83db1179c991846bef280f90de6deb1
Rasika Navarange <rn3717@ic.ac.uk>
2021-04-17 14:57:50 +0100

    Add doggos

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
-import { IonApp, IonLabel, IonList, IonItem, IonInput, IonToggle, IonRadio, IonCheckbox,
-  IonItemSliding, IonItemOption, IonItemOptions, IonContent } from '@ionic/react';
+import { Redirect, Route } from 'react-router-dom';
+import {
+  IonApp,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+} from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { ellipse, square, triangle } from 'ionicons/icons';
+import Tab2 from './pages/Tab2';
+import Tab3 from './pages/Tab3';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -18,49 +29,47 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
-
 /* Theme variables */
 import './theme/variables.css';
-import BasicSearchBar from "./components/BasicSearchBar";
-import CardExamples from "./components/CardExamples";
+
+import InfiniteScroll from "./pages/InfiniteScroll";
+import React from "react";
 
 const App: React.FC = () => (
-  <IonApp>
-      {/*-- Search Bar --*/}
-      <BasicSearchBar> </BasicSearchBar>
-
-      {/*-- User Card --*/}
-      <CardExamples> </CardExamples>
-
-    {/*-- List of Sliding Items --*/}
-    <IonList>
-      <IonItemSliding>
-        <IonItem>
-          <IonLabel>Item</IonLabel>
-        </IonItem>
-        <IonItemOptions side="end">
-          <IonItemOption onClick={() => {}}>Unread</IonItemOption>
-        </IonItemOptions>
-      </IonItemSliding>
-
-      <IonItemSliding>
-        <IonItem>
-          <IonLabel>Item</IonLabel>
-        </IonItem>
-        <IonItemOptions side="end">
-          <IonItemOption onClick={() => {}}>Unread</IonItemOption>
-        </IonItemOptions>
-      </IonItemSliding>
-    </IonList>
-  </IonApp>
+    <IonApp>
+      <IonReactRouter>
+        <IonTabs>
+          <IonRouterOutlet>
+            <Route exact path="/tab1">
+              <InfiniteScroll> </InfiniteScroll>
+            </Route>
+            <Route exact path="/tab2">
+              <Tab2 />
+            </Route>
+            <Route path="/tab3">
+              <Tab3 />
+            </Route>
+            <Route exact path="/">
+              <Redirect to="/tab1" />
+            </Route>
+          </IonRouterOutlet>
+          <IonTabBar slot="bottom">
+            <IonTabButton tab="tab1" href="/tab1">
+              <IonIcon icon={triangle} />
+              <IonLabel>Tab 1</IonLabel>
+            </IonTabButton>
+            <IonTabButton tab="tab2" href="/tab2">
+              <IonIcon icon={ellipse} />
+              <IonLabel>Tab 2</IonLabel>
+            </IonTabButton>
+            <IonTabButton tab="tab3" href="/tab3">
+              <IonIcon icon={square} />
+              <IonLabel>Tab 3</IonLabel>
+            </IonTabButton>
+          </IonTabBar>
+        </IonTabs>
+      </IonReactRouter>
+    </IonApp>
 );
 
 export default App;
-
-
-
-export const ListExample: React.FC = () => (
-    <IonContent>
-
-    </IonContent>
-);
